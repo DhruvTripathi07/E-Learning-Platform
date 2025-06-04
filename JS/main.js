d1f52ee031608b2ea25b0c20c2ab1877dd792d34
DhruvTripathi07 <dhruvkalakaar7@gmail.com>
2025-06-04 15:32:51 +0530

    E-Learning platform

---
+++ JS/main.js
@@ -0,0 +1,35 @@
+
+
+
+/* ---------------------------------------
+testimonial slider
+----------------------------------------*/ 
+function testimonialSlider() {
+    const carouselOne = document.getElementById('carouselOne');
+    if(carouselOne) {
+        carouselOne.addEventListener('slide.bs.carousel', function() {
+            const activeItem =this.querySelector(".active");
+            document.querySelector(".js-testimonial-img").src=
+            activeItem.getAttribute("data-js-testimonial-img");
+          })
+    }
+}
+testimonialSlider();
+
+// -------------------------------------------
+// Course preview video 
+// -------------------------------------------
+function coursePreviewVideo() {
+    const coursePreviewModal = document.querySelector(".js-course-preview-modal");
+    if(coursePreviewModal){
+        coursePreviewModal.addEventListener("shown.bs.modal", function(){
+            this.querySelector(".js-course-preview-video").play();
+            this.querySelector(".js-course-preview-video").currentTime = 0;
+        });
+
+        coursePreviewModal.addEventListener("hide.bs.modal", function(){
+            this.querySelector(".js-course-preview-video").pause();
+        })
+    }
+}
+coursePreviewVideo();
